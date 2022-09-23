db56997844a9254bc997891baa8f607cec5621c2
Brian Chen <brianntgal@gmail.com>
2022-09-22 21:27:10 -0400

    forgot to do this one

--- 03_py/list2/count_evens.py
+++ 03_py/list2/count_evens.py
@@ -1,2 +1,8 @@
 def count_evens(nums):
   return sum(i%2==0 for i in nums)
+
+t1 = [2, 1, 2, 3, 4]
+#t1 = [2, 2, 0]
+#t1 = [1, 3, 5]
+
+print(f"count_evens([{','.join(str(x) for x in t1)}]) is {str(count_evens(t1))}")
