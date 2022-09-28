3d26ce827ef3efd0e980916720e1825b7c7ae045
Brian Chen <bcchen52@gmail.com>
2022-09-28 12:09:38 -0400

    added comments

--- 05_bitstream/krewes.py
+++ 05_bitstream/krewes.py
@@ -6,11 +6,19 @@ with open('krewes.txt') as f:
 classes = {2:[],7:[],8:[]}
 
 def get_devo(krewes):
+    #split list into person
     person = krewes.split('@@@')
+
     for i in person:
+        #split each person's information into [pd,name,ducky]
         pinfo = i.split('$$$')
+
+        #classes[pd].append([name,ducky])
         classes[int(pinfo[0])].append([pinfo[1],pinfo[2]])
+
+    #pick pd from .keys()
     pd = r.choice(list(classes.keys()))
+
     person = r.choice(classes[pd])
     return(f"{pd} : {person[0]}, ducky named: {person[1]}")
 
