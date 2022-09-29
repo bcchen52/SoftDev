ce78defc11031c2ce9e5edc5110174b281e5e413
Brian Chen <bcchen52@gmail.com>
2022-09-29 09:13:06 -0400

    yass

--- 05_bitstream/krewes.py
+++ 05_bitstream/krewes.py
@@ -1,3 +1,20 @@
+'''
+Brian Chen 
+Soft Dev
+K05 -- bistream
+2022-09-29
+time spent: 30 mins
+
+DISCO:
+When you split a String, the String entry has a backslash n at the back. We manually remove it from the last entry with [:-1]. If not, there will be an extra line when you return.
+Use open('filename') as f to open a text file, and f.readline() to read a txt file with one line of text. 
+You can assign random to a variable, in this case r.
+
+QCC:
+...
+'''
+
+
 import random as r
 
 with open('krewes.txt') as f:
@@ -8,6 +25,7 @@ classes = {2:[],7:[],8:[]}
 def get_devo(krewes):
     #split list into person
     person = krewes.split('@@@')
+    person[-1] = person[-1][:-1]
 
     for i in person:
         #split each person's information into [pd,name,ducky]
