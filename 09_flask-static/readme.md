4484e22599d4a3202a9d1ab9e2f59d4b46792f21
Brian Chen <bcchen52@gmail.com>
2022-10-11 09:16:20 -0400

    yushi pushi

---
+++ 09_flask-static/readme.md
@@ -0,0 +1,5 @@
+QCC:
+
+When you run static/foo.html, it opens the html file in static. 
+
+static/foo downloads the text file.
---
+++ 09_hi/app.py
@@ -0,0 +1,21 @@
+# Brian Chen
+# SoftDev
+# K09 -- flask-static
+# 2022-10-11
+#
+
+# DEMO 
+# basics of /static folder
+
+from flask import Flask
+app = Flask(__name__) 
+
+@app.route("/")       
+def hello_world():
+    print("the __name__ of this module is... ")
+    print(__name__)
+    return "No hablo queso!"
+
+if __name__ == "__main__":  # true if this file NOT imported
+    app.debug = True        # enable auto-reload upon code change
+    app.run()
