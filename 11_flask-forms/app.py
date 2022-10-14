f1aae5a723f89eeb13faaa9d5d87da45e6d2d93d
Brian Chen <bcchen52@gmail.com>
2022-10-14 09:26:10 -0400

    hasj

--- 11_flask-forms/app.py
+++ 11_flask-forms/app.py
@@ -44,7 +44,7 @@ def disp_loginpage():
     return render_template( 'login.html' )
 
 
-@app.route("/auth") # , methods=['GET', 'POST'])
+@app.route("/auth") # , methods=[ 'POST'])
 def authenticate():
     print("\n\n\n")
     print("***DIAG: this Flask obj ***")
@@ -53,8 +53,8 @@ def authenticate():
     print(request)
     print("***DIAG: request.args ***")
     print(request.args)
-    #print("***DIAG: request.args['username']  ***")
-    #print(request.args['username'])
+    print("***DIAG: request.args['username']  ***")
+    print(request.args['username'])
     print("***DIAG: request.headers ***")
     print(request.headers)
     return "Waaaa hooo HAAAH"  #response to a form submission
