f28787071e7169e65012d98b63b10d6411bca6f2
bcchen52 <bcchen52@gmail.com>
2022-10-16 20:18:39 -0400

    added csv file for usernames

--- 11_flask-forms-xtra/app.py
+++ 11_flask-forms-xtra/app.py
@@ -2,10 +2,10 @@
 # SoftDev
 # Oct 2022
 
-import re
 from flask import Flask             #facilitate flask webserving
 from flask import render_template   #facilitate jinja templating
 from flask import request           #facilitate form submission
+import csv
 
 #the conventional way:
 #from flask import Flask, render_template, request
@@ -30,7 +30,14 @@ PROTIP: Insert your own in-line comments
 '''
 
 users = {}
-logged_in = False
+
+def get_users():
+    with open('users.csv','r') as csvfile:
+        csv_reader = csv.reader(csvfile, delimiter=',')
+        for x in csv_reader:
+            users[x[0]] = x[1] #puts csv file into dict
+
+get_users() #populates users for first time
 
 @app.route("/") #, methods=['GET', 'POST'])
 def disp_loginpage():
@@ -45,7 +52,7 @@ def disp_loginpage():
     #print(request.args['username'])
     print("***DIAG: request.headers ***")
     print(request.headers)
-    return render_template( 'login.html' )
+    return render_template( 'login.html',)
 
 
 @app.route("/auth") # , methods=[ 'POST'])
@@ -63,37 +70,54 @@ def authenticate():
     print(request.headers)
     newuser = request.args['username']
     password = request.args['password']
-    if newuser not in users.keys():
+    if newuser not in users.keys(): #check if user exists
         error = "User DNE"
         return render_template('login.html',
         error=error)
-    if users[newuser] != password:
+    if users[newuser] != password: #check if password matches user
         error = "Wrong Password"
         return render_template('login.html',
         error=error)
+    
     return render_template('home.html',
         user=newuser,
-        password=password,)  #response to a form submission
+        password=password,)  #redirects to home page
 
 @app.route("/signup", methods=['GET','POST'])
 def signup():
-    if request.method == 'GET':
-        return render_template('signup.html')
-    if request.method == 'POST':
+    if request.method == 'GET': #if opening this route
+        return render_template('signup.html') 
+    if request.method == 'POST': #if submitting information
         usernames = users.keys()
-        newuser = request.form.get('username')
+        newuser = request.form.get('username') #when using "POST" request.args DNE
         password = request.form.get('password')
         confirmation = request.form.get('confirmation')
-        if newuser in usernames:
+        if newuser in usernames: #check if user exists
             error = "Username already exists"
             return render_template('signup.html', 
-                error=error)
-        if password != confirmation:
+                error=error) #redirects back to page with error
+        if password != confirmation: 
             error = "PASSWORDS DO NOT MATCH!!!!!!"
             return render_template('signup.html', 
                 error=error)
-        users[newuser] = password
+
+        with open('users.csv','a') as csvfile: #if newuser works, add to csv
+            csv_writer = csv.writer(csvfile)
+            csv_writer.writerow([newuser,password])
+        get_users() #update local dict to match csv
+
         return render_template('login.html')
+
+@app.route("/logout")
+def logout():
+    return render_template( 'login.html' ) #brings back to homepage
+
+@app.route("/allusers")
+def allusers():
+    usernames = list(users.keys())
+    return render_template('allusers.html',
+        usernames=usernames)
+
 
 
 if __name__ == "__main__": #false if this file imported as module
