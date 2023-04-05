15a54ed5de005beced5fd41aa66d50b3fd6e7b42
Brian Chen <bcchen52@gmail.com>
2023-04-05 08:48:30 -0400

    s

---
+++ 27_js/fibfac.js
@@ -0,0 +1,19 @@
+// Team Briki :: Brian Chen, Mahir Riki
+// SoftDev pd2
+// K27 -- Basic functions in JavaScript
+// 2023-04-04t
+// --------------------------------------------------
+
+
+// as a duo...
+// pair programming style,
+// implement a fact and fib fxn
+
+<script src="biwascheme-0.7.3.js"></script>
+
+var code = '(fib 6)';
+var result = BiwaScheme.run(code);
+console.log(result);
+
+
+//Do whatever you think is needed. Think: S I M P L E.   Think: S M A R T.
