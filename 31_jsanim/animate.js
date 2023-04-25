901d96fd77853a601eafd991dddc92db3722a248
Brian Chen <bcchen52@gmail.com>
2023-04-25 09:23:38 -0400

    s

---
+++ 31_jsanim/animate.js
@@ -0,0 +1,45 @@
+var c = document.getElementById("playground");
+var dotButton = document.getElementById("buttonCircle");
+var stopButton = document.getElementById("buttonStop");
+
+var ctx = c.getContext("2d");
+
+ctx.fillstyle = "purple";
+
+var requestID;
+
+var clear = (e) => {
+    ctx.clearRect(0,0,500,500);
+    console.log("hi");
+};
+
+var radius = 0;
+var growing = true;
+
+var drawDot = () => {
+    //window.cancelAnimationFrame(requestID);
+    window.cancelAnimationFrame(requestID);
+    clear();
+    if (growing){
+        radius ++;
+    } else {
+        radius --;
+    }
+    if (radius == 250){
+        growing = false;
+    } else if (radius == 0){
+        growing = true;
+    }
+    ctx.arc(250,250, radius, 0, Math.PI * 2, true);
+    ctx.stroke();
+    console.log(radius);
+    
+    requestID = window.requestAnimationFrame(drawDot);
+};
+
+var stopIt = () => {
+    window.cancelAnimationFrame(requestID);
+};
+
+dotButton.addEventListener("click", drawDot);
+stopButton.addEventListener("click", stopIt);
