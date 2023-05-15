123ce5c8ec4e860bb672b96b2827ba2c84d9efea
bcchen52 <bcchen52@gmail.com>
2023-05-15 02:19:17 -0400

    js changes to p4

--- p4_stuff/app/static/js/map.js
+++ p4_stuff/app/static/js/map.js
@@ -11,5 +11,43 @@ var mapBounds = (e) => {
     console.log(bounds);
 };
 
+function getFlightPos(data){
+    var pos = [data[5], data[6]]
+    return pos;
+};
 
-map.addEventListener('click', mapBounds);
+function makeFlights(data){
+    flights = [];
+    for (let i = 0; i < data.length; i++) {
+        pos = getFlightPos(flightData[i]);
+        console.log(pos);
+        L.marker([pos[0],pos[1]]).addTo(map);
+    }
+};
+
+
+
+async function getData(bounds) {
+    var lamin = bounds["_southWest"]["lat"];
+    var lomin = bounds["_southWest"]["lng"];
+    var lamax = bounds["_northEast"]["lat"];
+    var lomax = bounds["_northEast"]["lng"];
+    const response = await fetch(`https://opensky-network.org/api/states/all?lamin=${lamin}&lomin=${lomin}&lamax=${lamax}&lomax=${lomax}`, {
+        method: "GET",
+    });
+    const jsonData = await response.json();
+    flightData = jsonData["states"];
+    console.log(flightData);
+    makeFlights(flightData);
+  };
+
+
+map.on('zoomend moveend', function () {
+    var bounds = map.getBounds();
+    console.log(bounds);
+    getData(bounds);
+});
+
+
+
+map.addEventListener('map.resize', mapBounds);
